467ed06905d5ce982afe09d942c54404796d2838
Rein de Vries <info@reinos.nl>
2015-03-16 21:16:12 +0100

    Update to ON()

--- jquery.enterKey.js
+++ jquery.enterKey.js
@@ -1,12 +1,12 @@
 (function ( $ ) { 
 	$.fn.enterKey = function (fnc) {
 	    return this.each(function () {
-	        $(this).keypress(function (ev) {
+	        $(document).on('keypress', $(this), function (ev) {
 	            var keycode = (ev.keyCode ? ev.keyCode : ev.which);
 	            if (keycode == '13') {
 	                fnc.call(this, ev, $(this));
 	            }
-	        })
-	    })
+	        });
+	    });
 	}
 }( jQuery ));
